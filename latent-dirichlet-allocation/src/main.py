6714e5871573d5f23e0bd24b9f86948fa0c25e73
rcuevass <rogercuevas.ca@gmail.com>
2020-04-06 23:53:33 -0400

    working code for tokenization and cleaning

--- latent-dirichlet-allocation/src/main.py
+++ latent-dirichlet-allocation/src/main.py
@@ -1,6 +1,7 @@
 import sys
 import re, numpy as np, pandas as pd
 from utils.logger import get_log_object
+from utils.data_processing import sent_to_words
 from pprint import pprint
 
 # Gensim
@@ -27,6 +28,7 @@ log.info('Stopwords obtained...')
 stop_words.extend(['from', 'subject', 're', 'edu', 'use', 'not', 'would', 'say', 'could', '_', 'be', 'know', 'good', 'go', 'get', 'do', 'done', 'try', 'many', 'some', 'nice', 'thank', 'think', 'see', 'rather', 'easy', 'easily', 'lot', 'lack', 'make', 'want', 'seem', 'run', 'need', 'even', 'right', 'line', 'even', 'also', 'may', 'take', 'come'])
 log.info('Stopwords extended...')
 
+'''
 # Import Dataset
 df = pd.read_json('https://raw.githubusercontent.com/selva86/datasets/master/newsgroups.json')
 log.info('dataframe obtained...')
@@ -35,5 +37,16 @@ log.info('saving dataframe to input data')
 df = df.loc[df.target_names.isin(['soc.religion.christian', 'rec.sport.hockey', 'talk.politics.mideast', 'rec.motorcycles']) , :]
 df.to_csv('../data/output/news_data_preprocessed.csv')
 log.info('saving dataframe to output data')
+'''
+
+log.info('Reading data')
+df = pd.read_csv('../data/output/news_data_preprocessed.csv')
+
+log.info('Converting to list')
+data = df.content.values.tolist()
+data_words = list(sent_to_words(data))
+log.info(str(data_words[:1]))
+
+
 
 
---
+++ latent-dirichlet-allocation/src/utils/data_processing.py
@@ -0,0 +1,11 @@
+import re
+import gensim
+
+
+def sent_to_words(sentences):
+    for sent in sentences:
+        sent = re.sub('\S*@\S*\s?', '', sent)  # remove emails
+        sent = re.sub('\s+', ' ', sent)  # remove newline chars
+        sent = re.sub("\'", "", sent)  # remove single quotes
+        sent = gensim.utils.simple_preprocess(str(sent), deacc=True)
+        yield sent
