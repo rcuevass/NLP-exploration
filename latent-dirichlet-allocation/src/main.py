3c33cdc4c8dad7db927e99dd41e1585c6462d62c
rcuevass <rogercuevas.ca@gmail.com>
2020-04-06 23:46:24 -0400

    commit to get data, logger added, gitignore updated

---
+++ latent-dirichlet-allocation/src/main.py
@@ -0,0 +1,39 @@
+import sys
+import re, numpy as np, pandas as pd
+from utils.logger import get_log_object
+from pprint import pprint
+
+# Gensim
+import gensim, logging, warnings
+import gensim.corpora as corpora
+from gensim.utils import lemmatize, simple_preprocess
+from gensim.models import CoherenceModel
+import matplotlib.pyplot as plt
+
+# NLTK Stop words
+
+import nltk
+#nltk.download('stopwords')
+
+from nltk.corpus import stopwords
+
+#nltk.download('stopwords')
+
+log = get_log_object()
+
+log.info('Getting stopwords...')
+stop_words = stopwords.words('english')
+log.info('Stopwords obtained...')
+stop_words.extend(['from', 'subject', 're', 'edu', 'use', 'not', 'would', 'say', 'could', '_', 'be', 'know', 'good', 'go', 'get', 'do', 'done', 'try', 'many', 'some', 'nice', 'thank', 'think', 'see', 'rather', 'easy', 'easily', 'lot', 'lack', 'make', 'want', 'seem', 'run', 'need', 'even', 'right', 'line', 'even', 'also', 'may', 'take', 'come'])
+log.info('Stopwords extended...')
+
+# Import Dataset
+df = pd.read_json('https://raw.githubusercontent.com/selva86/datasets/master/newsgroups.json')
+log.info('dataframe obtained...')
+df.to_csv('../data/input/news_data_raw.csv')
+log.info('saving dataframe to input data')
+df = df.loc[df.target_names.isin(['soc.religion.christian', 'rec.sport.hockey', 'talk.politics.mideast', 'rec.motorcycles']) , :]
+df.to_csv('../data/output/news_data_preprocessed.csv')
+log.info('saving dataframe to output data')
+
+
