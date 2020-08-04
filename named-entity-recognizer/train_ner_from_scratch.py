9b95269bce5723f201f80dcfc429ac0839398fb1
rcuevass <rogercuevas.ca@gmail.com>
2020-08-04 12:32:58 -0400

    add functions to tag data

--- named-entity-recognizer/train_ner_from_scratch.py
+++ named-entity-recognizer/train_ner_from_scratch.py
@@ -1,5 +1,11 @@
 from spacy.gold import GoldParse
 from spacy.scorer import Scorer
+from spacy import displacy
+from spacy.util import minibatch, compounding
+import plac
+import random
+import warnings
+from pathlib import Path
 
 
 def evaluate_ner(spacy_ner_model, list_labeled_examples: list):
@@ -13,3 +19,32 @@ def evaluate_ner(spacy_ner_model, list_labeled_examples: list):
         scorer.score(pred_value, gold)
 
     return scorer.scores
+
+
+def tag_all_text(text_in: str, word_to_tag: str):
+    len_word = len(word_to_tag)
+    # get initial positions
+    matches = plac.re.finditer(word_to_tag, text_in)
+    initial_positions = [match.start() for match in matches]
+    # final positions
+    list_positions = [(x, x + len_word) for x in initial_positions]
+    return list_positions
+
+
+def get_all_tagged_sentence_many_entity(text_in: str, entity_dictionary: dict):
+    list_entities_positions = []
+    list_words = entity_dictionary.keys()
+    for word_ in list_words:
+        # get list of positions for given entities
+        aux_list_positions = tag_all_text(text_in, word_)
+        # get instance
+        aux_instance = entity_dictionary[word_]
+        # update list with instance
+        list_positions_instance = [(x[0], x[1], aux_instance) for x in aux_list_positions]
+        # update list
+        list_entities_positions.extend(list_positions_instance)
+
+    dict_to_tag = {'entities': list_entities_positions}
+    tagged_sentence = (text_in, dict_to_tag)
+
+    return tagged_sentence
